91ee6f387224beae12eddcccb1e85518e00610b3
Mirko Kontroller <Mirko_Kontroller@proton.me>
2026-09-13 01:06:56 +0200

    Refactor script.js to enhance functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*Elements===============================================================================*/
+var output = document.getElementById("output_text");
+
 var send_field = document.getElementById("input_1");
 var receive_field = document.getElementById("output_1")
 
@@ -19,4 +22,30 @@ function apply_animation(animation, duration, element){
     animation_elements.push(animation_element);
 }
 
+apply_animation("spin", "2s", "receive_button");
+
+/*Buttons================================================================================*/
+function get_button(button_id, action) {
+    var button = document.getElementById(button_id);
+
+    button.addEventListener("click", action);
+}
+
+    //Send_button
+function pub_message(){
+    var input = document.getElementById("input_1");
+    var message = input.value;
+
+    client.publish('latest_message', message, {qos: 2, retain: true});
+}
+
+get_button("send_button", pub_message);
+
 /*MQTT===================================================================================*/
+
+client.on('message', function(topic, message){
+    var text = message.toString();
+    if(topic === 'latest_message'){
+        output.innerHTML = text;
+    }
+})
