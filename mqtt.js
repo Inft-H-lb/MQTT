4b062bc5475a74dab2714b83949e9d0de06f022e
Mirko Kontroller <Mirko_Kontroller@proton.me>
2026-09-13 01:05:21 +0200

    Add files via upload

---
+++ mqtt.js
@@ -0,0 +1,20 @@
+var broker = 'wss://d34704fe8f874a3aa342016af4877888.s1.eu.hivemq.cloud:8884/mqtt';
+
+var options = {
+    username : 'Mirko Kontroller',
+    password : '#Inft_H_lb/MQTT1#',
+    clientId : 'Galaxy_Tab_E',
+    clean : true
+};
+
+var client = mqtt.connect(broker, options);
+
+client.on('connect', function(){
+    alert("VERBUNDEN");
+});
+
+client.on('error', function(){
+    alert("SCHEISSE");
+});
+
+client.subscribe('latest_message', {qos: 2});
